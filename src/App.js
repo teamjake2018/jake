782c32f760be2b5d69ae9178af41950cc2fa7788
AdamWier <38018787+AdamWier@users.noreply.github.com>
2018-11-03 18:20:57 +0100

    trying to use REACT_APP prefix

--- src/App.js
+++ src/App.js
@@ -2,6 +2,21 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+const Clarifai = require('clarifai');
+const app = new Clarifai.App({
+    apiKey: '76229e50dfab4ed38208640297183b43'
+   });
+
+   app.models.initModel({id: Clarifai.GENERAL_MODEL, version: "aa7f35c01e0642fda5cf400f543e7c40"})
+        .then(generalModel => {
+          return generalModel.predict("https://samples.clarifai.com/metro-north.jpg");
+        })
+        .then(response => {
+          var concepts = response['outputs'][0]['data']['concepts'];
+          console.log(concepts);
+        })
+
 class App extends Component {
   render() {
     return (
