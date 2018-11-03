ad6ebd71d12f216a4ecd2bf9185df24359c9475f
AdamWier <38018787+AdamWier@users.noreply.github.com>
2018-11-03 17:16:33 +0100

    possible installed a bunch of unnecessary dependencies, but JSON is appearing in terminal when node bin/dev is typed

--- public/index.js
+++ public/index.js
@@ -1,1 +1,13 @@
-console.log(process.env.SECRET_MESSAGE)
+const Clarifai = require('clarifai');
+const app = new Clarifai.App({
+    apiKey: process.env.API_KEY
+   });
+
+   app.models.initModel({id: Clarifai.GENERAL_MODEL, version: "aa7f35c01e0642fda5cf400f543e7c40"})
+        .then(generalModel => {
+          return generalModel.predict("https://samples.clarifai.com/metro-north.jpg");
+        })
+        .then(response => {
+          var concepts = response['outputs'][0]['data']['concepts'];
+          console.log(concepts);
+        })
