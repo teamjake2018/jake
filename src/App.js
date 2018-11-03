8e7d1d4f83676b9594c23ac4de2c117ac86d865c
Kelby-Baca <kelbybaca@yahoo.com>
2018-11-03 15:39:49 -0500

    Added header with inputs.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  searchHandler = (event) => {
+    event.preventDefault();
+    console.log("Searching!");
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header>
+          <h1>Scavenger Hunt!</h1>
+          <form onSubmit={this.searchHandler}>
+            <input type="file" /><input type="submit" value="Search with file" /><br/>
+            <input type="text" placeholder="Image URL here!" /><input type="submit" value="Search with URL" />
+          </form>
         </header>
       </div>
     );
