3c80d78cefcb9eb0846fc64fb49a8a66d31a58dc
AdamWier <38018787+AdamWier@users.noreply.github.com>
2018-11-04 02:59:40 +0100

    uncommented right code

--- src/components/Tags.js
+++ src/components/Tags.js
@@ -24,7 +24,7 @@ class Tags extends Component {
 
   apiCall = () => {
     console.log("called")
-    /*app.workflow.predict('{trainedTibetanSpaniel}', "https://samples.clarifai.com/metro-north.jpg").then(response => {
+    app.workflow.predict('{trainedTibetanSpaniel}', "https://samples.clarifai.com/metro-north.jpg").then(response => {
       var concepts = response["outputs"][0]["data"]["concepts"];
       console.log({ concepts });
       const names = concepts.map(elm => elm.name);
@@ -37,10 +37,10 @@ class Tags extends Component {
     })
     .then( names => {
       this.setState({ tags: names });
-    });*/
+    });
 
 
-    app.models
+    /*app.models
       .initModel({
         id: Clarifai.GENERAL_MODEL,
         version: "aa7f35c01e0642fda5cf400f543e7c40"
@@ -63,7 +63,7 @@ class Tags extends Component {
       })
       .then( names => {
         this.setState({ tags: names });
-      });
+      });*/
   };
 
   componentDidMount() {
