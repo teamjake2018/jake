e7bf89d7ffe9ac2baa773c8e8ff86e786f06ab52
Eunice Park <28845173+eunicode@users.noreply.github.com>
2018-11-08 21:44:57 -0800

    :art: Style form inputs, buttons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,32 @@
 import React from 'react';
+import '../styles/Header.css';
 
 const header = props => {
     return (
         <header>
-          <div class="header-container">
+          <div className='header-container'>
           <h1>Scavenger Hunt!</h1>
           <form onSubmit={props.submit}>
-            <input 
-              type="file" 
-              name="file"
-              id="file"
-              accept='image/png, image/jpeg'
-              ref={props.fileInputRef}
-              />
-              <label for="file">Choose a file</label>
-            <input type="submit" value="Search with file" />
-            <br/>
-            <span>or</span><input type="text" placeholder="Image URL here!" ref={props.urlInputRef} />
-            <input type="submit" value="Search with URL" />
+            <div className='file-container'>
+              <span>Choose a file</span>
+              <label for='file' className='file-select'>
+                  Click to choose a file!
+              </label>
+              <input 
+                type="file" 
+                name="file"
+                id="file"
+                accept='image/png, image/jpeg'
+                ref={props.fileInputRef} 
+                className='input-file'/>
+              <input type="submit" value="Search with file" className='input-file-submit'/>
+            </div> {/* .file-container */}
+            {/* <br/> */}
+            <div className='url-container'>
+              <span>or</span>
+              <input type="text" placeholder="Image URL here!" ref={props.urlInputRef} className='input-url'/>
+              <input type="submit" value="Search with URL" className='input-url-submit'/>
+            </div> {/* .url-container */}
           </form>
           </div>
         </header>
