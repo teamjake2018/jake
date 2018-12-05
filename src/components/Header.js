b8b635464dd11a12949ac68742efe1f1626fbee0
Eunice Park <28845173+eunicode@users.noreply.github.com>
2018-12-05 00:44:24 -0800

    style: format code with prettier (#32)
    Move App.test.js file

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,40 +1,58 @@
 import React from 'react';
 import '../styles/Header.css';
 
-const header = props => {
-    return (
-        <header>
-          <div className='header-container'>
-          <h1>Scavenger Hunt!</h1>
-          <form onSubmit={props.submit}>
-            <div className='header-container-row'>
-              <span>Choose a file</span>
-              <div className='file-container-col2'>
-                <label htmlFor='file' className='file-select'>
-                    Click to choose a file!&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                </label>
-                <input 
-                  type="file" 
-                  name="file"
-                  id="file"
-                  accept='image/png, image/jpeg'
-                  ref={props.fileInputRef} 
-                  className='input-file'/>
-                <input type="submit" value="Search with file&nbsp;&nbsp;" className='input-file-submit'/>
-              </div> {/* .file-container-col2 */}
-            </div> {/* .file-container */}
-            {/* <br/> */}
-            <div className='header-container-row'>
-              <span>or</span>
-              <div className='ur-container-col2'>
-                <input type="text" placeholder="Image URL here!" ref={props.urlInputRef} className='input-url'/>
-                <input type="submit" value="Search with URL" className='input-url-submit'/>
-              </div> {/* .url-container-col2 */}
-            </div> {/* .url-container */}
-          </form>
+const Header = props => {
+  return (
+    <header>
+      <div className="header-container">
+        <h1>Scavenger Hunt!</h1>
+        <form onSubmit={props.submit}>
+          <div className="header-container-row">
+            <span>Choose a file</span>
+            <div className="file-container-col2">
+              <label htmlFor="file" className="file-select">
+                Click to choose a file!&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              </label>
+              <input
+                type="file"
+                name="file"
+                id="file"
+                accept="image/png, image/jpeg"
+                ref={props.fileInputRef}
+                className="input-file"
+              />
+              <input
+                type="submit"
+                value="Search with file&nbsp;&nbsp;"
+                className="input-file-submit"
+              />
+            </div>
+            {/* .file-container-col2 */}
           </div>
-        </header>
-    );
-}
+          {/* .file-container */}
+          {/* <br/> */}
+          <div className="header-container-row">
+            <span>or</span>
+            <div className="ur-container-col2">
+              <input
+                type="text"
+                placeholder="Image URL here!"
+                ref={props.urlInputRef}
+                className="input-url"
+              />
+              <input
+                type="submit"
+                value="Search with URL"
+                className="input-url-submit"
+              />
+            </div>
+            {/* .url-container-col2 */}
+          </div>
+          {/* .url-container */}
+        </form>
+      </div>
+    </header>
+  );
+};
 
-export default header;
+export default Header;
